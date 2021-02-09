590f4f284f736cfe94bd790f3bbcb3eea34adfe9
Mehkel <anna.karutina@khk.ee>
2021-02-09 10:51:46 +0200

    Paigaldab wordpressi koos LAMP teenustega

--- LAMP_install.sh
+++ LAMP_install.sh
@@ -1,6 +1,7 @@
-APACHE2=$(dpkg-query -W -f='${Status}' apache2>/dev/null | grep -c 'ok installed')
+APACHE2=$(dpkg-query -W -f='${Status}' apache2>/dec/null | grep -c 'ok installed')
 MYSQL=$(dpkg-query -W -f='${Status}' mysql-server>/dev/null | grep -c 'ok installed')
 PHP=$(dpkg-query -W -f='${Status}' php7.0 2>/dev/null | grep -c 'ok installed')
+WORDPRESS=$(dpkg-query -W -f='${Status}' wordpress>/dev/null | grep -c 'ok installed')
 
 if [ $APACHE2 -eq 0 ]; then
 	echo "paigaldame apache2"
@@ -17,9 +18,16 @@ if [ $MYSQL -eq 0 ]; then
 	echo "host = localhost" >> $HOME/.my.cnf
 	echo "user = root" >> $HOME/.my.cnf
 	echo "password = qwerty" >> $HOME/.my.cnf
+
 if [ $PHP -eq 0 ]; then
 	echo "paigaldame php"
 	apt install php7.0 libapache2-mod-php7.0 php7.0-mysql -y
+	echo "PHP on paigaldatud"
+
+if [ $WORDPRESS -eq 0 ]; then
+	echo "Paigaldame wordpressi"
+	wget https://wordpress.org/latest.tar.gz
+	tar -xzvf latest.tar.gz
 	echo "PHP on paigaldatud"
 
 elif [ $APACHE2 -eq 1 ]; then
@@ -34,6 +42,10 @@ elif [ $MYSQL -eq 1 ]; then
 elif [ $PHP -eq 1 ]; then
 	echo "PHP on juba paigaldatud"
 	which php
+
+elif [ $WORDPRESS -eq 1 ]; then
+	echo "WordPress on juba paigaldatud"
+fi
 fi
 fi
 fi
